d8d82f1f40135806cbb75611b7eaeef25bcdeaa2
Javier Rodriguez <j.r.juan2001@gmail.com>
2024-09-24 13:20:42 +0200

    Changed model name

--- detect.sh
+++ detect.sh
@@ -4,4 +4,4 @@
 # last frame captured in order to check that YOLOv9 is receiving the correct input. Ensure that the IP of the camera
 # from the RTSP URL is the correct one to avoid problems
 
-python detect.py --weights weights/best.pt --source 'rtsp://admin:admin12345@192.168.1.102:554/live' --name test
+python detect.py --weights weights/yolov9_best.pt --source 'rtsp://admin:admin12345@192.168.1.102:554/live' --name test
